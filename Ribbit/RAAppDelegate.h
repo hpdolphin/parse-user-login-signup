0743920e4a584846b48a4548f6d00003a2c5d578
Lailei Huang (Alex) <laileihuang@gmail.com>
2014-07-16 10:55:56 +1000

    Initial Commit

---
+++ Ribbit/RAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RAAppDelegate.h
+//  Ribbit
+//
+//  Created by Lailei Huang on 16/07/2014.
+//  Copyright (c) 2014 alex.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Lailei Huang on 16/07/2014.
+//  Copyright (c) 2014 alex.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RAAppDelegate class]));
+    }
+}
