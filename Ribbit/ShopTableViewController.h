244e41cf4d20e74f32a20eb667517328fa4633ec
Lailei Huang (Alex) <laileihuang@gmail.com>
2014-07-18 10:31:35 +1000

    add ShopTableViewController and setup initial UI

---
+++ Ribbit/ShopTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShopTableViewController.h
+//  Ribbit
+//
+//  Created by Lailei Huang on 18/07/2014.
+//  Copyright (c) 2014 alex.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShopTableViewController : UITableViewController
+
+@end
